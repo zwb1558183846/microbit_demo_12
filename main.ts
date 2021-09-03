fc38d385ab893b3614a3f5215704a9055458dbee
zwb1558183846 <1558183846@qq.com>
2021-09-03 20:15:08 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,77 @@
+let azimuth = 0
+input.calibrateCompass()
+basic.forever(function () {
+    azimuth = input.compassHeading()
+    if (azimuth >= 0 && azimuth < 45) {
+        basic.showLeds(`
+            # # # # .
+            # # . . .
+            # . # . .
+            # . . # .
+            . . . . #
+            `)
+    }
+    if (azimuth >= 45 && azimuth < 90) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (azimuth >= 90 && azimuth < 135) {
+        basic.showLeds(`
+            . . . . #
+            # . . # .
+            # . # . .
+            # # . . .
+            # # # # .
+            `)
+    }
+    if (azimuth >= 135 && azimuth < 180) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (azimuth >= 180 && azimuth < 225) {
+        basic.showLeds(`
+            # . . . .
+            . # . . #
+            . . # . #
+            . . . # #
+            . # # # #
+            `)
+    }
+    if (azimuth >= 225 && azimuth < 270) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (azimuth >= 270 && azimuth < 315) {
+        basic.showLeds(`
+            . # # # #
+            . . . # #
+            . . # . #
+            . # . . #
+            # . . . .
+            `)
+    }
+    if (azimuth >= 315 && azimuth < 360) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
